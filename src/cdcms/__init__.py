553332da75c6b84306a74ccef070cf6a9db14e06
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2023-07-10 08:33:58 +0530

    removed cron crap and added requirements file

--- src/cdcms/__init__.py
+++ src/cdcms/__init__.py
@@ -2,15 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_apscheduler import APScheduler
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cdcms.db'
 app.config['SECRET_KEY'] = '9f479d5aebc024b17588bd87'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
 login_manager = LoginManager(app)
 login_manager.login_view = 'home_page'
 login_manager.login_message_category = 'info'
